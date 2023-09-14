5d409948b76cc8860c4d453ed23930aa0759b321
tamasvencel <tamasvenci46@gmail.com>
2023-09-14 19:46:21 +0300

    env vars should be declared on host site

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-const path = "./config.env";
-dotenv.config({ path: path });
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
-
-console.log(dotenv.config());
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
